e38ef54f77d3f3cf90e7dea31e2a6ab166ed616b
Elvis Guanoluisa <elviserock@gmail.com>
2023-05-30 07:18:13 -0500

    archivos rama modulos

---
+++ fundamentos.mjs
@@ -0,0 +1,11 @@
+// import { login, valorConstante } from './module.mjs';
+// login();
+// console.log(valorConstante);
+
+
+// import login from "./module.mjs";
+// login()
+
+
+import {login} from "./module.mjs";
+login()
---
+++ module.mjs
@@ -0,0 +1,21 @@
+// export const login = (username, password) => {
+//     if (!username && !password) return console.log("Ingreso no autorizado 1");
+//     console.log(`Bienvenido ${username}`);
+// };
+// export const valorConstante = 99;
+
+
+// const login = (username,password)=>{
+//     if(!username && !password) return console.log("Ingreso no autorizado 2")
+//     console.log(`Bienvenido ${username}`)
+// }
+// export default login
+
+
+const login = (username,password)=>{
+    if(!username && !password) return console.log("Usuario no autorizado 3")
+    console.log(`Bienvenido ${username}`)
+}
+export {
+    login
+}
